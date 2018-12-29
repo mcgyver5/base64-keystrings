21a1a7b7048ad11f78640d50d0ef6471a393883d
mcgyver5 <tim.e.mcguire@gmail.com>
2018-12-29 00:53:41 +0000

    adding tests for debug

--- base64-keystrings-tests.py
+++ base64-keystrings-tests.py
@@ -16,6 +16,13 @@ class Base64Tests(unittest.TestCase):
         self.assertEqual(lines[0],"U3RyaW5nIHBhc3N3b3JkID0gIkhPUlNFIFNUQVBMRSBCQVRURVJZIjsK")
         self.assertEqual(lines[1],"cmluZyBwYXNzd29yZCA9ICJIT1JTRSBTVEFQTEUgQkFUVEVSWSI7")
         self.assertEqual(lines[2],"dHJpbmcgcGFzc3dvcmQgPSAiSE9SU0UgU1RBUExFIEJBVFRFUlki")
-
+    def test_debug(self):
+        proc = subprocess.Popen("python base64-keystrings.py -d -f plain.txt", stdout=subprocess.PIPE, shell=True)
+        (out,err) = proc.communicate()
+        lines = out.splitlines()
+        answerlines = lines[-3:]
+        self.assertEqual(answerlines[0],"U3RyaW5nIHBhc3N3b3JkID0gIkhPUlNFIFNUQVBMRSBCQVRURVJZIjsK")
+        self.assertEqual(answerlines[1],"cmluZyBwYXNzd29yZCA9ICJIT1JTRSBTVEFQTEUgQkFUVEVSWSI7")
+        self.assertEqual(answerlines[2],"dHJpbmcgcGFzc3dvcmQgPSAiSE9SU0UgU1RBUExFIEJBVFRFUlki")
 if __name__ == '__main__':
     unittest.main()
